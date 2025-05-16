7e7822fba9a90ae29c50b71a76d4a7a75a391b7b
berlene <berlenebernabe12@gmail.com>
2025-05-16 17:59:28 +0800

    MOB - POLICY UPDATED

--- mobile/mobileapp/components/PolicyScreen.js
+++ mobile/mobileapp/components/PolicyScreen.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View, Text, FlatList } from 'react-native';
 import styles from './styles/PolicyStyle';
 import Header from './Header';
-
 
 export default function PolicyScreen({ navigation }) {
   const policies = [
@@ -12,39 +10,39 @@ export default function PolicyScreen({ navigation }) {
     { id: '3', title: 'Access Policy', description: 'Only authorized users can access restricted areas.' },
     { id: '4', title: 'Cleanliness', description: 'Maintain cleanliness and dispose of waste properly.' },
     { id: '5', title: 'Emergency Procedures', description: 'Know the emergency exits and first aid locations.' },
+    {
+      id: '6',
+      title: 'Laboratory Request and Return Policy',
+      description:
+        'LABORATORY REQUESTS MUST BE SUBMITTED (7) SEVEN DAYS BEFORE THE SCHEDULED DATE NEEDED. FAILURE TO ADHERE TO THIS PROTOCOL WILL RESULT IN NON-FULFILLMENT OF THE REQUEST.\n\n' +
+        'RETURN THE BORROWED ITEMS TO THE LABORATORY STOCK ROOM. MICROSCOPES, GLASSWARES, AND EQUIPMENT MUST BE CLEANED AND IN INTACT CONDITION.\n\n' +
+        'DIRTY, MISSING, BROKEN, OR EXTREMELY DAMAGED LABORATORY ITEMS SHOULD BE REPORTED TO THE LABORATORY CUSTODIAN FOR PROPER DOCUMENTATION.'
+    },
   ];
 
   const [headerHeight, setHeaderHeight] = useState(0);
-  
+
   const handleHeaderLayout = (event) => {
     const { height } = event.nativeEvent.layout;
     setHeaderHeight(height);
   };
+
   return (
     <View style={styles.container}>
-    <Header onLayout={handleHeaderLayout} />
-  <View style={[styles.wholeSection,{ marginTop: headerHeight }]}>
-    <Text style={styles.title}>POLICY</Text>
-      <FlatList
-        data={policies}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <Text style={styles.cardTitle}>{item.title}</Text>
-            <Text style={styles.cardDescription}>{item.description}</Text>
-          </View>
-          
-        )}
-      />
+      <Header onLayout={handleHeaderLayout} />
+      <View style={[styles.wholeSection, { marginTop: headerHeight }]}>
+        <Text style={styles.title}>POLICY</Text>
+        <FlatList
+          data={policies}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.cardTitle}>{item.title}</Text>
+              <Text style={styles.cardDescription}>{item.description}</Text>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
 }
-
-      {/* Back Button */}
-      {/* <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-          <Icon name="arrow-left" size={30} color="white" />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Policies</Text>
-      </View> */}
