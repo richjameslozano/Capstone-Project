e2365036888c3a40f3a4acaa1c3ea2c59fcf6cd4
RichiRich0928 <henreizharuta@gmail.com>
2025-06-02 00:34:48 +0800

    MOB - ITEM LIST PANGIT FOR NOW

--- mobile/mobileapp/components/admin/ItemListScreen.js
+++ mobile/mobileapp/components/admin/ItemListScreen.js
@@ -1,10 +1,11 @@
 // ItemListScreen.js
-import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, TouchableOpacity, ActivityIndicator } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect, useCallback } from "react";
+import { View, Text, FlatList, TouchableOpacity, ActivityIndicator, StatusBar } from "react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../../backend/firebase/FirebaseConfig";
 import styles from "../styles/adminStyle/ItemListStyle";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Header from "../Header";
 
 const ItemListScreen = () => {
@@ -12,6 +13,19 @@ const ItemListScreen = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
 
+      const [headerHeight, setHeaderHeight] = useState(0);
+    const handleHeaderLayout = (event) => {
+      const { height } = event.nativeEvent.layout;
+      setHeaderHeight(height);
+    };
+
+    useFocusEffect(
+      useCallback(() => {
+        StatusBar.setBarStyle('dark-content');
+        StatusBar.setBackgroundColor('transparent'); // Android only
+        StatusBar.setTranslucent(true)
+      }, [])
+    );
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -41,7 +55,7 @@ const ItemListScreen = () => {
 const renderItem = ({ item }) => (
     <TouchableOpacity style={styles.itemCard} onPress={() => handleItemPress(item)}>
       <Text style={styles.itemName}>{item.itemName}</Text>
-      <Text style={styles.details}>Qty: {item.quantity} </Text>
+      <Text style={styles.details}>Inventory Balance: {item.quantity} </Text>
     </TouchableOpacity>
   );
 
@@ -49,16 +63,36 @@ const renderItem = ({ item }) => (
     return <ActivityIndicator size="large" color="#007bff" style={{ flex: 1, justifyContent: "center" }} />;
   }
 
+
+
   return (
     <View style={styles.container}>
-      <Header/>
+      <View style={styles.inventoryStocksHeader} onLayout={handleHeaderLayout}>
+          <TouchableOpacity onPress={() => navigation.navigate('Admin2Dashboard')} style={styles.backButton}>
+                          <Icon name="keyboard-backspace" size={28} color="black" />
+                        </TouchableOpacity>
+
+        <View>
+          <Text style={{textAlign: 'center', fontWeight: 800, fontSize: 18, color: '#395a7f'}}>Update Item Stocks</Text>
+          <Text style={{ fontWeight: 300, fontSize: 13, textAlign: 'center'}}>Scan Item to Update</Text>
+        </View>
+
+          <TouchableOpacity style={{padding: 2}}>
+            <Icon name="information-outline" size={24} color="#000" />
+          </TouchableOpacity>
+        </View>
+
+    <View style={{flex: 1, backgroundColor: 'white', marginTop: headerHeight+5,padding: 10, }}>
       <Text style={styles.title}>All Inventory Items</Text>
+      <Text style={{color: 'gray', marginBottom: 10}}>Select an Item</Text>
       <FlatList
         data={items}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
-        contentContainerStyle={{ paddingBottom: 20 }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20, padding: 10}}
       />
+    </View>
     </View>
   );
 };
--- mobile/mobileapp/components/styles/adminStyle/ItemListStyle.js
+++ mobile/mobileapp/components/styles/adminStyle/ItemListStyle.js
@@ -1,18 +1,33 @@
-import { StyleSheet } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingHorizontal: 16,
-    paddingTop: 16
+    backgroundColor: "#e9ecee",
+    paddingHorizontal: 7,
   },
 
+    inventoryStocksHeader:{
+      position: 'absolute',
+          backgroundColor: '#fff',
+          flex: 1,
+          paddingTop: StatusBar.currentHeight+15,
+          left: 0,
+          right:0,
+          flexDirection: 'row',
+          paddingBottom: 10,
+          paddingHorizontal: 15,
+          alignItems: 'center',
+          justifyContent:'space-between',
+          borderBottomWidth: 1,
+          borderColor: '#e9ecee',
+          zIndex: 999,
+    },
+
   title: {
-    fontSize: 22,
+    fontSize: 19,
     fontWeight: "bold",
-    marginBottom: 12,
-    marginTop: 100,
+    marginBottom: 5,
   },
 
   itemCard: {
@@ -30,6 +45,8 @@ export default StyleSheet.create({
 
   details: {
     fontSize: 14,
-    color: "#555"
-  }
+    color: "gray",
+    fontWeight: 300
+  },
+
 });
