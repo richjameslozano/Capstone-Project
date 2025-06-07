228878a9d69a8b952ce9791ecf4e319a045fcd38
berlene <berlenebernabe12@gmail.com>
2025-06-07 14:51:52 +0800

    WEB - ADDED ENV

--- web/website/src/config.js
+++ web/website/src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    SECRET_KEY: "f9a8b6c4d2e09f3b1c7d6a5e8f4c2b0d1e3a9b7f5d8c6e0a3c1f7d9b2a4e6c8"
+    SECRET_KEY: process.env.REACT_APP_SECRET_KEY
   };
   
   export default CONFIG;
