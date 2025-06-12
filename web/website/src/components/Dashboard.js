75018821fbe00b6741b25ed249e148049dbc09b3
richjameslozano <richjameslozano@gmail.com>
2025-06-12 16:12:22 +0800

    hehe

--- web/website/src/components/Dashboard.js
+++ web/website/src/components/Dashboard.js
@@ -1248,8 +1248,28 @@ useEffect(() => {
     </Col>
   </Row>
 </Tabs.TabPane>
+
+<Tabs.TabPane tab="Analytics" key="3">
+
+ 
+  <Row gutter={[24, 24]} justify="center">
+    <Col xs={24} style={{ width: '100%' }}>
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+    <Card title="Calendar" style={{ width: '100%', minHeight: 600 }}>
+          <div style={{ minHeight: 500, width: '100%' }}>
+          </div>
+        </Card>
+      </div>
+    </Col>
+  </Row>
+</Tabs.TabPane>x
+
+
+
+
 </Tabs>
 </Content>
+
                     
          <SuccessModal isVisible={showModal} onClose={closeModal} />
 
