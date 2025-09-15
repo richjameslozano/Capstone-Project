8f07b17fddb667e8f02ed6803ceb18a46775352b
berlene <berlenebernabe12@gmail.com>
2025-09-15 22:04:45 +0800

    WEB & MOB - SUBREQ UNCLAIMED COMMENT ADDED

--- web/website/src/components/users/HistoryLog.js
+++ web/website/src/components/users/HistoryLog.js
@@ -1837,6 +1837,15 @@ const renderUnclaimedTab = () => {
                     </div>
                   </div>
 
+                  {/* Unclaimed Comment */}
+                  {item.fullData.unclaimedComment && (
+                    <div style={{ marginTop: 10, padding: 10, backgroundColor: "#f5f5f5", borderRadius: 6, border: "1px solid #d9d9d9" }}>
+                      <p style={{ margin: 0, fontSize: 14, fontWeight: "500", color: "#333" }}>
+                        <strong>Reason:</strong> {item.fullData.unclaimedComment}
+                      </p>
+                    </div>
+                  )}
+
                   {/* Contact Info */}
                   <div style={{ marginTop: 10, textAlign: "center", padding: 10, backgroundColor: "#fff0e6", borderRadius: 6 }}>
                     <p style={{ margin: 0, fontSize: 14, color: "#d84315", fontWeight: "500" }}>
@@ -2624,6 +2633,12 @@ const handlePrint = () => {
   {selectedLog.action !== 'Request Rejected' && (
     <Descriptions.Item label="Note">
       {selectedLog.reason || 'N/A'}
+    </Descriptions.Item>
+  )}
+
+  {selectedLog.action === 'Unclaimed' && selectedLog.unclaimedComment && (
+    <Descriptions.Item label="Unclaimed Reason">
+      {selectedLog.unclaimedComment}
     </Descriptions.Item>
   )}
 </Descriptions>
